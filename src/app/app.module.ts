5b78834b71a3ec2b2d576f1fc07d568eafc355eb
Greg Sullivan <gregsull@cox.net>
2019-12-08 14:10:25 -0700

    Attempting warningalert component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { WarningAlertComponent } from './warningalert/warningalert.component.ts'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WarningAlertComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
