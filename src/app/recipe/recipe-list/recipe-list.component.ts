f4742c6f5d099657ef1b29ab0114bba4c7efbd19
Greg Sullivan <gregsull@cox.net>
2019-12-09 20:51:22 -0700

    Adding Recipe Model

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes = Recipe[] = [
+    new Recipe('TestRecipe', 'This is only a test', 'URL to a recipe image goes here'),
+    new Recipe('TestRecipe2', 'This is another test', 'URL to second recipe impage goes here')
+  ];
 
   constructor() { }
 
